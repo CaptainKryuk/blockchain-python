9af1562050b5a620f4a1da7045d909a9157ef249
CaptainKryuk <bestrongwb@gmail.com>
2018-05-20 01:02:31 -0700

    final

--- app/main.py
+++ app/main.py
@@ -1,21 +1,31 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask import request
-from app import block
+from app.block import *
 
 app = Flask(__name__)
 
 
-@app.route('/', method=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
 
     if request.method == 'POST':
+
         lender = request.form['lender']
         amount = request.form['amount']
         borrower = request.form['borrower']
 
-        block.write_block(name=lender, amount=amount, to_whom=borrower)
+        write_block(name=lender, amount=amount, to_whom=borrower,)
+        return redirect(url_for('index'))
+
     return render_template('index.html')
+
+
+@app.route('/checking/', methods=['GET'])
+def check():
+    results = check_integrity()
+
+    return render_template('index.html', results=results)
 
 
 if __name__ == '__main__':
